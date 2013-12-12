9183e23100741b2bb11a510ea949db339423aae5
Agustin Anfuso <anfusoagustin89@gmail.com>
2013-12-12 02:04:59 -0300

    Columna para franja horaria de entrega en admin.

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -7,6 +7,10 @@ Spree::Order.class_eval do
   validate :end_delivery_time, presence: true, allow_nil: false
   validate :delivery_time_specific_validation
 
+  def delivery_time
+    "#{start_delivery_time.strftime("%H:%M")} - #{end_delivery_time.strftime("%H:%M")}"
+  end
+
   def delivery_time_specific_validation
     if start_delivery_time && end_delivery_time && ['payment', 'confirm', 'complete'].include?(state)
       if end_delivery_time <= start_delivery_time
--- app/overrides/admin_orders_delivery_time_column.rb
+++ app/overrides/admin_orders_delivery_time_column.rb
@@ -3,6 +3,7 @@ Deface::Override.new(virtual_path: "spree/admin/orders/index",
                      insert_top: "[data-hook='admin_orders_index_headers']",
                      partial: "spree/admin/orders/delivery_time_header",
                      disabled: false)
+
 Deface::Override.new(virtual_path: "spree/admin/orders/index",
                      name: "add_delivery_time_table_row_cell",
                      insert_top: "[data-hook='admin_orders_index_rows']",

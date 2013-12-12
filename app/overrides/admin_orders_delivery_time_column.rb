d46ab40193ae8898bece25de831e659e062f8c49
Agustin Anfuso <anfusoagustin89@gmail.com>
2013-12-12 01:49:18 -0300

    No hashrockets

--- app/overrides/admin_orders_delivery_time_column.rb
+++ app/overrides/admin_orders_delivery_time_column.rb
@@ -1,10 +1,10 @@
-Deface::Override.new(:virtual_path => "spree/admin/orders/index",
-                     :name => "add_delivery_time_table_header",
-                     :insert_top => "[data-hook='admin_orders_index_headers']",
-                     :partial => "spree/admin/orders/delivery_time_header",
-                     :disabled => false)
-Deface::Override.new(:virtual_path => "spree/admin/orders/index",
-                     :name => "add_delivery_time_table_row_cell",
-                     :insert_top => "[data-hook='admin_orders_index_rows']",
-                     :partial => "spree/admin/orders/delivery_time_cell",
-                     :disabled => false)
+Deface::Override.new(virtual_path: "spree/admin/orders/index",
+                     name: "add_delivery_time_table_header",
+                     insert_top: "[data-hook='admin_orders_index_headers']",
+                     partial: "spree/admin/orders/delivery_time_header",
+                     disabled: false)
+Deface::Override.new(virtual_path: "spree/admin/orders/index",
+                     name: "add_delivery_time_table_row_cell",
+                     insert_top: "[data-hook='admin_orders_index_rows']",
+                     partial: "spree/admin/orders/delivery_time_cell",
+                     disabled: false)
--- app/overrides/delivery_time_details.rb
+++ app/overrides/delivery_time_details.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path => "spree/shared/_order_details",
-                     :name => "add_delivery_time_details_to_order",
-                     :insert_before => "[data-hook='order_details']",
-                     :partial => "spree/orders/order_delivery_time_details",
-                     :disabled => false)
+Deface::Override.new(virtual_path: "spree/shared/_order_details",
+                     name: "add_delivery_time_details_to_order",
+                     insert_before: "[data-hook='order_details']",
+                     partial: "spree/orders/order_delivery_time_details",
+                     disabled: false)

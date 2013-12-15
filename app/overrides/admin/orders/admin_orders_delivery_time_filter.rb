bec09c6b6a9ca0d3fdddb43aeefb14f1178666f1
Agustin Anfuso <anfusoagustin89@gmail.com>
2013-12-15 01:13:26 -0300

    Without filters.

--- app/overrides/admin/orders/admin_orders_delivery_time_filter.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(virtual_path: "spree/admin/orders/index",
-                     name: "add_delivery_time_filter_to_orders_index",
-                     insert_after: ".date-range-filter",
-                     partial: "spree/admin/orders/delivery_time_filter",
-                     disabled: false)
